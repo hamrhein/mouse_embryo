93f756cdb9ce60bc909f97959688cf45a05c2461
hamrhein <hamrhein+github@caltech.edu>
2020-01-09 11:01:38 -0800

    add option and support for output_dir

--- util/build_sql_stringdb_database.py
+++ util/build_sql_stringdb_database.py
@@ -5,8 +5,10 @@ import argparse
 from sql import chunked_file, build_sql_stringdb_database
 
 def main(args):
+    output_file = "{}/{}".format(args.output_dir, args.output_file)
+    
     build_sql_stringdb_database(args.alias_file, args.evidence_file,
-            args.actions_file, args.output_file)
+            args.actions_file, output_file)
 
 
 if __name__ == "__main__":
@@ -14,6 +16,7 @@ if __name__ == "__main__":
     parser.add_argument("alias_file", help="example: 10090.protein.aliases.v10.5.txt.gz")
     parser.add_argument("evidence_file", help="example: 10090.protein.links.detailed.v10.5.txt.gz")
     parser.add_argument("actions_file", help="example: 10090.protein.actions.v10.5.txt.gz")
+    parser.add_argument("output_dir", default=".", help="directory to write output into")
     parser.add_argument("output_file", help="database file to output")
 
     args = parser.parse_args()
