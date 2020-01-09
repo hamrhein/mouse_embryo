856d0078c0130c554600b94cf24c1854f0fa3370
Henry Amrhein <hamrhein@caltech.edu>
2020-01-09 15:58:33 -0800

    changed examples to reflect up to date versions

--- util/build_sql_stringdb_database.py
+++ util/build_sql_stringdb_database.py
@@ -13,9 +13,9 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Build a database for SDBL")
-    parser.add_argument("alias_file", help="example: 10090.protein.aliases.v10.5.txt.gz")
-    parser.add_argument("evidence_file", help="example: 10090.protein.links.detailed.v10.5.txt.gz")
-    parser.add_argument("actions_file", help="example: 10090.protein.actions.v10.5.txt.gz")
+    parser.add_argument("alias_file", help="example: 10090.protein.aliases.v11.0.txt.gz")
+    parser.add_argument("evidence_file", help="example: 10090.protein.links.detailed.v11.0.txt.gz")
+    parser.add_argument("actions_file", help="example: 10090.protein.actions.v11.0.txt.gz")
     parser.add_argument("output_file", help="database file to output")
     parser.add_argument("--output_dir", default=".", help="directory to write output into")
 
